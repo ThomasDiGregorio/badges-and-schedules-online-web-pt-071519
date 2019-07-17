5a5fe42ff14f9fb0346350a7f784e969d0757152
salubrious-pipe-1423 <digregoriothomas@yahoo.com>
2019-07-17 03:49:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,16 +1,14 @@
 # Write your code here.
 
+def badge_maker(speaker)
+ return "Hello, my name is #{speaker}."
+end
 
+speakers_lineup = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-
-
-
-
-
-
-
-
-
+def batch_badge_creator(speakers_lineup)
+ speakers_lineup.collect {|speaker| badge_maker(speaker)}
+end
 
 def assign_rooms(speakers_lineup)
  speakers_lineup.each_with_index.map {|speaker, index| "Hello, #{speaker}! You'll be assigned to room #{index+1}!"}
